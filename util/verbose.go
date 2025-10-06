65d04a5dc2360f4ef1cef33823b112a7f0a6f0de
Marco van Eck <67060615+marcovaneck@users.noreply.github.com>
2025-10-06 12:43:26 +0200

    More verbose

--- util/verbose.go
+++ util/verbose.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 )
 
+var verbose_flag = flag.Bool("v",false,"Do verbose logging")
+
 var verbose *bool = nil
 
 func Verbose() bool {
 	if verbose == nil {
-		if flag.Parsed() {
-			verbose = flag.Bool("v",GetEnvBool("VERBOSE",false),"Do verbose logging")
+		if *verbose_flag {
+			verbose = verbose_flag
+		} else if env_file.initialized  {
+			*verbose = GetEnvBool("VERBOSE",false)
 		} else {
 			return false
 		}
